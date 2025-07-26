c8d0e7b50ab2373e4556c0cacf030b86e69c5e5a
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2025-07-26 16:31:41 +0200

    Setup the most basic button handler

--- Assets/Editor/SimpleCustomEditor.cs
+++ Assets/Editor/SimpleCustomEditor.cs
@@ -20,11 +20,29 @@ public class SimpleCustomEditor : EditorWindow
         VisualElement root = rootVisualElement;
 
         // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        // Notes: creating elements in code is not recommended for complex UIs, prefer using UXML.
+        // Doing so prevent using the UI Builder to design the UI.
         VisualElement label = new Label("Hello World! From C#");
         root.Add(label);
 
         // Instantiate UXML
-        VisualElement labelFromUXML = m_VisualTreeAsset.Instantiate();
-        root.Add(labelFromUXML);
+        VisualElement elementsFromUXML = m_VisualTreeAsset.Instantiate();
+        root.Add(elementsFromUXML);
+
+        SetupButtonHandler();
+    }
+
+    void SetupButtonHandler()
+    {
+        // Find the button in the UXML and set up a click handler
+        Button myButton = rootVisualElement.Q<Button>("button1");
+        if (myButton != null)
+        {
+            myButton.clicked += () => Debug.Log("Button clicked!");
+        }
+        else
+        {
+            Debug.LogError("Button with name 'button1' not found in UXML.");
+        }
     }
 }
