e34c09805e2534d369fe7f7549ccedfc9e996e38
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2025-07-27 14:52:32 +0200

    Simplify and modernize the code using var

--- Assets/Editor/SimpleCustomEditor.cs
+++ Assets/Editor/SimpleCustomEditor.cs
@@ -10,24 +10,21 @@ public class SimpleCustomEditor : EditorWindow
     [MenuItem("Window/UI Toolkit/Editor Windows/Simple Custom Editor")]
     public static void ShowExample()
     {
-        SimpleCustomEditor wnd = GetWindow<SimpleCustomEditor>();
-        wnd.titleContent = new GUIContent("Simple Custom Editor");
+        var window = GetWindow<SimpleCustomEditor>();
+        window.titleContent = new GUIContent("Simple Custom Editor");
     }
 
     public void CreateGUI()
     {
-        // Each editor window contains a root VisualElement object
-        VisualElement root = rootVisualElement;
-
         // VisualElements objects can contain other VisualElement following a tree hierarchy.
         // Notes: creating elements in code is not recommended for complex UIs, prefer using UXML.
         // Doing so prevent using the UI Builder to design the UI.
-        VisualElement label = new Label("Hello World! From C#");
-        root.Add(label);
+        var label = new Label("Hello World! From C#");
+        rootVisualElement.Add(label);
 
         // Instantiate UXML
-        VisualElement elementsFromUXML = m_VisualTreeAsset.Instantiate();
-        root.Add(elementsFromUXML);
+        var elementsFromUxml = m_VisualTreeAsset.Instantiate();
+        rootVisualElement.Add(elementsFromUxml);
 
         SetupButtonHandler();
     }
@@ -35,8 +32,8 @@ public class SimpleCustomEditor : EditorWindow
     void SetupButtonHandler()
     {
         // Find the button in the UXML and set up a click handler
-        Button button1 = rootVisualElement.Query<Button>("button1");
-        button1.RegisterCallback<ClickEvent>(OnButtonClick);
+        var button = rootVisualElement.Q<Button>("button1");
+        button.RegisterCallback<ClickEvent>(OnButtonClick);
     }
 
     void OnButtonClick(ClickEvent evt)
@@ -44,8 +41,8 @@ public class SimpleCustomEditor : EditorWindow
         Debug.Log("Button was clicked!");
         m_ClickCount++;
 
-        Label label = rootVisualElement.Query<Label>("label1");
-        Toggle toggle = rootVisualElement.Query<Toggle>("toggle1");
+        var label = rootVisualElement.Q<Label>("label1");
+        var toggle = rootVisualElement.Q<Toggle>("toggle1");
         if (toggle.value == true)
             label.text = "Button clicked " + m_ClickCount + " times.";
         else
